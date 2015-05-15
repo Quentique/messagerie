c8a0a12c84fa51c7a0dcb6daffd9256f13593df4
Quentin DE MUYNCK <quentique@outlook.com>
2015-05-15 23:03:18 +0200

    ADD: Ajout du UNDO et affichage messages supprimés

--- messagerie.php
+++ messagerie.php
@@ -239,6 +239,13 @@ editor.setReadOnly(true);
 		}
 			
 	}
+	elseif ($_GET['action'] == 'undo')
+	{
+	
+	global $wpdb;
+	$wpdb->update('wp_messagerie', array('dossier' => 'inbox'), array('id' => $_GET['mail']), array('%s'), array('%s'));
+	echo '<meta http-equiv="refresh" content="0;URL=?page=messagerie"/>';
+	}
 	
 }
 }
