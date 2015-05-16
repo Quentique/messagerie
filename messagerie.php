8e3fa5864e49ef9a613e4ee83379a0f0d6f3b150
Quentin DE MUYNCK <quentique@outlook.com>
2015-05-16 15:35:15 +0200

    Ajout : Suppression des brouillons de la DB

--- messagerie.php
+++ messagerie.php
@@ -264,6 +264,19 @@ editor.setReadOnly(true);
 	$wpdb->update('wp_messagerie', array('dossier' => 'inbox'), array('id' => $_GET['mail']), array('%s'), array('%s'));
 	echo '<meta http-equiv="refresh" content="0;URL=?page=messagerie"/>';
 	}
+	elseif($_GET['action'] == 'deldraft')
+	{
+	 if(!isset($_GET['mail']) || empty($_GET['mail']))
+		{
+		 echo '<meta http-equiv="refresh" content="0;URL=?page=messagerie&action=draft"/>';
+		}
+		global $wpdb;
+		if (get_current_user_id() == $wpdb->get_var($wpdb->prepare('SELECT sender FROM wp_messagerie WHERE id = %d', $_GET['mail'])))
+		{
+		  $wpdb->query($wpdb->prepare('DELETE FROM wp_messagerie WHERE id = %d', $_GET['mail']));
+		}
+		 echo '<meta http-equiv="refresh" content="0;URL=?page=messagerie&action=draft"/>';
+	}
 	
 }
 }
