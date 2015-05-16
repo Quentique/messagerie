a180e5dce3d4c57ec60867dc3f427a27601a3099
Quentin DE MUYNCK <quentique@outlook.com>
2015-05-16 10:40:14 +0200

    COMPLETE Ajout du FORWARD complet

--- messagerie.php
+++ messagerie.php
@@ -70,6 +70,7 @@ if ($_GET['action'] == read)
 {
 ?>
 <li><a href="?page=messagerie&action=answer&mail=<?php echo $_GET['mail'];?>"><?php _e('Answer', 'messagerie'); ?></a></li>
+<li><a href="?page=messagerie&action=forward&mail=<?php echo $_GET['mail'];?>"><?php _e('Forward', 'messagerie'); ?></a></li>
 <?php } ?>
 </ul>
 </nav>
