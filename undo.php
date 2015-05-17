d51b9a51afd331c42d6b96f4f9779b8c2cec3f3c
Quentin DE MUYNCK <quentique@outlook.com>
2015-05-17 07:29:57 +0200

    Indentation de 'delete.php', de 'undo.php', de 'delete_draft.php'

--- undo.php
+++ undo.php
@@ -1,3 +1,4 @@
-<?php global $wpdb;
+<?php 
+	global $wpdb;
 	$wpdb->update('wp_messagerie', array('dossier' => 'inbox'), array('id' => $_GET['mail']), array('%s'), array('%s'));
 	echo '<meta http-equiv="refresh" content="0;URL=?page=messagerie"/>';
