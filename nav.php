66b30959170061a765f1ab0fbd1379a8399c1569
Quentin DE MUYNCK <quentique@outlook.com>
2015-05-17 07:20:53 +0200

    Indentation de 'nav.php' et de 'sidebar.php'

--- nav.php
+++ nav.php
@@ -1,12 +1,13 @@
 <nav>
-<ul id="messagerie_nav">
-<li><a href="?page=messagerie&action=compose"><?php _e('New', 'messagerie'); ?></a></li>
-<?php 
-if ($_GET['action'] == read)
-{
-?>
-<li><a href="?page=messagerie&action=answer&mail=<?php echo $_GET['mail'];?>"><?php _e('Answer', 'messagerie'); ?></a></li>
-<li><a href="?page=messagerie&action=forward&mail=<?php echo $_GET['mail'];?>"><?php _e('Forward', 'messagerie'); ?></a></li>
-<?php } ?>
-</ul>
+	<ul id="messagerie_nav">
+		<li><a href="?page=messagerie&action=compose"><?php _e('New', 'messagerie'); ?></a></li>
+		<?php 
+		if ($_GET['action'] == read)
+		{
+		?>
+		<li><a href="?page=messagerie&action=answer&mail=<?php echo $_GET['mail'];?>"><?php _e('Answer', 'messagerie'); ?></a></li>
+		<li><a href="?page=messagerie&action=forward&mail=<?php echo $_GET['mail'];?>"><?php _e('Forward', 'messagerie'); ?></a></li>
+		<?php
+		} ?>
+	</ul>
 </nav>
