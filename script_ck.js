abb0618e6fea9dad6411c15bd71e943a6d4e7e47
Quentin DE MUYNCK <quentique@outlook.com>
2015-08-06 10:14:43 +0200

    Ajout de l'image du loader

--- script_ck.js
+++
@@ -1,7 +0,0 @@
-var editor; 
-CKEDITOR.replace('txt-aref');
-CKEDITOR.on( 'instanceReady', function( ev ) { 
-editor = ev.editor; 
-editor.setReadOnly(false);
-}
-);
--- script_ck2.js
+++
@@ -1,8 +0,0 @@
-			CKEDITOR.replace('test2'); 
-			var editor;
-
-			CKEDITOR.on( 'instanceReady', function( ev ) {
-			editor = ev.editor;
-			editor.setReadOnly(true);
-
-		});
